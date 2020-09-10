1679de59070dea0c51b21aba3345200e29b80737
hunter-evans <hunter_d_evans@yahoo.com>
2020-09-09 22:34:51 -0400

    Finished custom splitting/regex of CSV records.

--- CSVParser.java
+++ CSVParser.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -107,11 +108,15 @@ public class CSVParser {
 		numReceived++;
 		
 		// Split line
-		// TODO: Figure out proper regex
-		String[] lineParts = line.split(",");
+		ArrayList<String> lineParts = customSplit(line);
+
+		// for(String s : lineParts) {
+		// 	System.out.println(s);
+		// }
+		// System.out.println("");
 
 		// If there are the correct number of entries
-		if (lineParts.length == numHeaders) {
+		if (lineParts.size() == numHeaders) {
 
 			// Increment the number of successful records
 			numSuccessful++;
@@ -126,6 +131,73 @@ public class CSVParser {
 			badPW.println(line);
 
 		}
+
+	}
+
+	/**
+	 *
+	 */
+	public static ArrayList<String> customSplit(String line) {
+
+			// Create expandable list
+			ArrayList<String> result = new ArrayList<String>();
+
+			// Create buffer to contain characters
+			StringBuffer curVal = new StringBuffer();
+
+			// Determines if we are in quotes or not.
+			boolean inQuotes = false;
+
+			// Convert line into iterable array
+			char[] chars = line.toCharArray();
+
+			// Iterate over the characters in the line
+			for (char ch : chars) {
+
+				if (inQuotes) {
+
+					// Move out of inQuote mode if second quote encountered
+					if (ch == '"') {
+						inQuotes = false;
+					}
+
+					// Always add the character to the buffer (even quotes)
+					curVal.append(ch);
+				}
+				else {
+
+					// When delimiter encountered, add to list and flush the buffer
+					if (ch == ',') {
+
+						result.add(curVal.toString());
+						curVal = new StringBuffer();
+
+					}
+					else {
+
+						// Move into inQuote mode for first quote encountered
+						if (ch == '"') {
+							inQuotes = true;
+						} 
+						// Only break when end of line encountered
+						else if (ch == '\r' || ch == '\n') {
+							break;
+						}
+
+						// Always add the character to the buffer (even quotes)
+						curVal.append(ch);
+
+					}
+
+				}
+
+			}
+
+			// Add the last buffer to the list
+			result.add(curVal.toString());
+
+			// Return the list
+			return result;
 
 	}
 
